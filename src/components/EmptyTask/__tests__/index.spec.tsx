f7ec2acb882c6d8d2eafc7b150e0e54149b06eca
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-07-23 21:01:08 -0300

    test: add test empty list

---
+++ src/components/EmptyTask/__tests__/index.spec.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import { EmptyTask } from "..";
+
+describe("<EmptyTask />", () => {
+  it("should be render correctly", () => {
+    const { getByText } = render(<EmptyTask />);
+    expect(getByText("Você ainda não tem tarefas cadastradas"));
+  });
+});
