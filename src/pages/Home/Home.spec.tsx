36699d67d59f59db6dabaa3563c7fe554e28b0bc
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-07-25 18:44:34 -0300

    test: implement tests home

--- src/pages/Home/Home.spec.tsx
+++ src/pages/Home/Home.spec.tsx
@@ -1,11 +1,17 @@
-import { act, fireEvent, render, waitFor } from "@testing-library/react-native";
+import { fireEvent, render, waitFor } from "@testing-library/react-native";
 import { Home } from ".";
-import { getStorageTasks, setStorageTask } from "../../libs/asyncStorage";
-import { mockTasks } from "../../__mocks__/task/mockTasks";
+import { getStorageTasks } from "../../libs/asyncStorage";
+import { mockOneTask, mockTasks } from "../../__mocks__/task/mockTasks";
 import {
   BUTTON_TEST_ID,
   INPUT_TEST_ID,
 } from "../../components/InputCreateTask";
+import {
+  BUTTON_COMPLETE_TASK_TEST_ID,
+  BUTTON_REMOVE_TASK_TEST_ID,
+  TASK_ITEM_COMPLETED_TEST_ID,
+  TASK_ITEM_CREATED_TEST_ID,
+} from "../../components/TaskItem";
 
 jest.mock("../../libs/asyncStorage", () => ({
   getStorageTasks: jest.fn(),
@@ -41,6 +47,30 @@ describe("<Home/>", () => {
 
     await waitFor(() => {
       expect(getByText(taskName)).toBeTruthy();
+    });
+  });
+  it("should complete task", async () => {
+    const task = [mockOneTask];
+    mockedGetStorageTasks.mockResolvedValue(task);
+    const { getByTestId } = render(<Home />);
+    await waitFor(() => {
+      expect(getByTestId(TASK_ITEM_CREATED_TEST_ID)).toBeTruthy();
+    });
+    fireEvent.press(getByTestId(BUTTON_COMPLETE_TASK_TEST_ID));
+    await waitFor(() => {
+      expect(getByTestId(TASK_ITEM_COMPLETED_TEST_ID)).toBeTruthy();
+    });
+  });
+  it("should be remove task", async () => {
+    const task = [mockOneTask];
+    mockedGetStorageTasks.mockResolvedValue(task);
+    const { getByTestId, getByText } = render(<Home />);
+    await waitFor(() => {
+      expect(getByText("Task 1")).toBeTruthy();
+    });
+    fireEvent.press(getByTestId(BUTTON_REMOVE_TASK_TEST_ID));
+    await waitFor(() => {
+      expect(getByText("Você ainda não tem tarefas cadastradas")).toBeTruthy();
     });
   });
 });
